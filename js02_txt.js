0fba04fd94ce4d895da7cbe9e0605edccd70ea42
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-01-30 20:02:34 -0500

    fix

--- js02_txt.js
+++ js02_txt.js
@@ -20,21 +20,6 @@ const TRAVEL_COST = 2 //cost of travel per mile
 window.addEventListener("load", setupForm)
 
 // set the form's default values
-function setupForm() {
-  document.getElementById("photoNum").value = 1
-  document.getElementById("photoHrs").value = 2
-  document.getElementById("makeBook").checked = false
-  document.getElementById("photoRights").checked = false
-  document.getElementById("photoDist").value = 0
-  getEstimate()
-
-  // Add event handlers for each input control
-  document.getElementById("photoNum").onchange = getEstimate
-  document.getElementById("photoHrs").onchange = getEstimate
-  document.getElementById("photoDist").onchange = getEstimate
-  document.getElementById("makeBook").onchange = getEstimate
-  document.getElementById("photoRights").onchange = getEstimate
-}
 
 // estimate the total cost of the service
 function getEstimate() {
@@ -58,4 +43,19 @@ function getEstimate() {
 
   // Display the total cost estimate
   document.getElementById("estimate").innerHTML = "$" + totalCost
+}
+function setupForm() {
+  document.getElementById("photoNum").value = 1
+  document.getElementById("photoHrs").value = 2
+  document.getElementById("makeBook").checked = false
+  document.getElementById("photoRights").checked = false
+  document.getElementById("photoDist").value = 0
+  getEstimate()
+
+  // Add event handlers for each input control
+  document.getElementById("photoNum").onchange = getEstimate
+  document.getElementById("photoHrs").onchange = getEstimate
+  document.getElementById("photoDist").onchange = getEstimate
+  document.getElementById("makeBook").onchange = getEstimate
+  document.getElementById("photoRights").onchange = getEstimate
 }
